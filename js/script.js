74829db0c751409d431d0019f4bca5dc26421991
gdu49 <gdu49@escuela.banoqabil.pk>
2023-05-14 17:03:58 +0500

    FEAT: todo list

--- js/script.js
+++ js/script.js
@@ -1,63 +1,78 @@
-//Assignment 1
-alert("Error! Please enter a valid password.");
-alert("Welcome To Qabil Web!\nGood Luck!")
-alert("Welcome To Qabil Web!")
-alert("Good Luck!")
+// //Assignment 1
+// alert("Error! Please enter a valid password.");
+// alert("Welcome To Qabil Web!\nGood Luck!")
+// alert("Welcome To Qabil Web!")
+// alert("Good Luck!")
 
+//Assignment 2
+var username;
+var myName = "Syed Ammar Ahmed";
+var message = "Hello World";
+alert(message);
 
-//Task 1
-var zakatPercent = 0.025
-var userAsset = + prompt ("Asset Value")
-var result = zakatPercent*userAsset
-alert("Your Zakat="+result)
+var usrName = prompt('What is your name?');
+var usrAge = prompt('What is your age?');
+var usrProfession = prompt('What is your Proffession?');
+var usrData = alert('The User Named, ' + usrName + ' is ' + usrAge + ' Years Old and have profession of ' + usrProfession + '.')
 
-
-//Task 2
-var fitrah;
-var familyMembers;
-var type;
-
-//types in rs per person
-var flour = 320;
-var barley = 480;
-var dates = 2800;
-var raisin = 6400;
-
-familyMembers = prompt("How many family members?");
-type = prompt("Which type? \n1. flour \n2. barley \n3.dates \n4.raisin")
-
-switch(type) {
-    case "1":
-      fitrah = flour * familyMembers;
-      break;
-    case "2":
-        fitrah = barley * familyMembers;
-      break;
-    case "3":
-        fitrah = dates * familyMembers;
-      break;
-    case "4":
-        fitrah = raisin * familyMembers;
-      break;;
+var p = 'PIZZA';
+for (let i = 0; i < 5; p--) {
+  
 }
 
-alert("Your Fitrah =" + fitrah +"Rs.")
+// //Task 1
+// var zakatPercent = 0.025
+// var userAsset = + prompt ("Asset Value")
+// var result = zakatPercent*userAsset
+// alert("Your Zakat="+result)
 
-//Task 3
-var secretNo = 3;
-var usrGuess;
 
-usrGuess = prompt("Guess the Secret No. from a Range of 1-10:");
-if (usrGuess === secretNo){
-	alert("Congatulations! You have guessed the secret no.");
-}else if (usrGuess > secretNo ){
-	alert("Incorrect! Guess is high, please try again.");
-}else if (usrGuess < secretNo ){
-	alert("Incorrect! Guess is low, please try again.");
-}
+// //Task 2
+// var fitrah;
+// var familyMembers;
+// var type;
 
-//Task 4
+// //types in rs per person
+// var flour = 320;
+// var barley = 480;
+// var dates = 2800;
+// var raisin = 6400;
 
-var usrName = prompt("Please Enter Your Name:")
+// familyMembers = prompt("How many family members?");
+// type = prompt("Which type? \n1. flour \n2. barley \n3.dates \n4.raisin")
+
+// switch(type) {
+//     case "1":
+//       fitrah = flour * familyMembers;
+//       break;
+//     case "2":
+//         fitrah = barley * familyMembers;
+//       break;
+//     case "3":
+//         fitrah = dates * familyMembers;
+//       break;
+//     case "4":
+//         fitrah = raisin * familyMembers;
+//       break;;
+// }
+
+// alert("Your Fitrah =" + fitrah +"Rs.")
+
+// //Task 3
+// var secretNo = 3;
+// var usrGuess;
+
+// usrGuess = prompt("Guess the Secret No. from a Range of 1-10:");
+// if (usrGuess === secretNo){
+// 	alert("Congatulations! You have guessed the secret no.");
+// }else if (usrGuess > secretNo ){
+// 	alert("Incorrect! Guess is high, please try again.");
+// }else if (usrGuess < secretNo ){
+// 	alert("Incorrect! Guess is low, please try again.");
+// }
+
+// //Task 4
+
+// var usrName = prompt("Please Enter Your Name:")
 
 
---
+++ js/todo.js
@@ -0,0 +1,13 @@
+var itemadd;
+// var itemList = document.getElementById("itemList");
+// console.log(itemList);
+
+createLi= document.createElement("li");
+addClassInLi= createLi.setAttribute("class","list-group-item");
+console.log(createLi);
+console.log(addClassInLi);
+
+function getItem(itemadd) {
+    console.log(itemadd);
+    //alert('Item Added Successfully!')
+}
